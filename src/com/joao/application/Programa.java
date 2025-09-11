0ca380b8fe7c36c336e0a5e548183819d3589f31
Jhon7-dev <joaovict27@icloud.com>
2025-09-11 10:59:03 -0300

    algoritmo quase pronto,falta apenas ajuste de métodos(toString)

---
+++ src/com/joao/application/Programa.java
@@ -0,0 +1,95 @@
+package com.joao.application;
+
+import java.util.Scanner;
+
+import com.joao.entities.Vetor;
+ 
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		
+		
+		System.out.print("Quantos convidados serão cadastrados? ");
+		int c = scn.nextInt();
+		Vetor vetor = new Vetor(c);
+		int op = 0;
+		
+		do {
+			
+		
+		System.out.println("==== FESTA SUMMER ELETRO ====");
+		System.out.println("Lista de Convidados");
+		System.out.println("1-Cadastrar convidados");
+		System.out.println("2-Listar todos os convidados");
+		System.out.println("3-Buscar convidado por posição");
+		System.out.println("4-Buscar convidado pelo nome");
+		System.out.println("5-Inserir convidado");
+		System.out.println("6-Remover convidado de uma posição");
+		System.out.println("7-Ver quantidade atual de convidados");
+		System.out.println("8-Sair");
+		System.out.println("=====================================");
+		System.out.println("");
+	    op  = scn.nextInt();
+		scn.nextLine();
+		switch(op) {
+		case 1: {
+			System.out.println("=== Cadastros de convidados ===");
+			System.out.println("Digite o nome: ");
+			String nome = scn.nextLine();
+			vetor.adiciona(nome);
+			System.out.println("Nome cadastrado com sucesso!");
+			break;
+	    	}
+		case 2:{
+			System.out.println("Lista de convidados");
+			System.out.println(vetor.toString());
+			break;
+		}
+		case 3:{ 
+			System.out.println("Busca de convidados");
+			System.out.print("Digite o convidado pela posição: ");
+			int posicao = scn.nextInt();
+			System.out.println(vetor.busca(posicao)); 
+			System.out.println(vetor);
+			break;
+		}
+		case 4:{	
+			System.out.println("Busca de convidado por nome");
+			System.out.print("Digete o nome: ");
+			String nome = scn.nextLine();
+			System.out.println(vetor.busca(nome));
+			System.out.println(vetor);
+			break;
+		}
+		case 5:{
+			System.out.println("Inserir convidado em uma posição especifica");
+			System.out.println();
+			break;
+		}
+		case 6: {
+			System.out.println("Remoção de convidado de uma posição");
+			System.out.print("o convidado de qual posição você deseja remover da lista ? ");
+			int pos = scn.nextInt();
+			scn.nextLine();
+			vetor.remove(pos);
+			System.out.println(vetor);
+			System.out.println();
+			break;
+		}
+		case 7:{
+			System.out.println("=== Quantidade de atual de convidados ====");
+			vetor.getTamanho();
+			break;
+		}
+		case 8:{
+			System.out.println("Programa encerrando........");
+			break;
+		}
+	}
+		
+	}while(op != 8);
+		System.out.println("Programa encerrado, boas festas!");
+	}
+}
--- src/com/joao/entities/Vetor.java
+++ src/com/joao/entities/Vetor.java
@@ -1,5 +1,9 @@
 package com.joao.entities;
 
+import java.util.Arrays;
+
+import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+
 public class Vetor {
 	private String [] elementos;
 	private int tamanho = 0;
@@ -19,25 +23,50 @@ public class Vetor {
 		}
 	}
 	public String busca(int posicao) {
-		for(int i = 0:i<this.elementos.length;i++) {
-			
+		if(!(posicao>=0 && posicao<tamanho)) {
+			throw new IllegalArgumentException("posição inválida!");
 		}
+		return elementos[posicao]; // elementos (nome do convidado na posiçao tal)
+	}
+	
+	public int busca(String elemento) {
+		for(int i = 0; i < this.tamanho;i++) {
+			if(elementos[i].equals(elemento)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public void adiciona(int posicao, String elemento) {
+		if(posicao<0 && posicao>tamanho) {
+			throw new IllegalArgumentException("Posição inválida!");
+		}
+		for(int i = tamanho - 1;i>=posicao;i--) {
+			this.elementos[i+1] = elementos[i];
+		}
+		this.elementos[posicao] = elemento;
+	    this.tamanho++;
 	}
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public void remove(int posicao) {
+		if(!(posicao>=0 && posicao<=tamanho)) {
+			throw new IllegalArgumentException("Posição inválida");
+		}
+		for(int i = posicao;i<this.tamanho-1;i++) {
+			this.elementos[i] = this.elementos[i+1];
+		}
+		this.tamanho --;
+	}
+		
+	//public void 
+	@Override
+	public String toString() {
+		return "Vetor [elementos=" + Arrays.toString(elementos) + ", tamanho=" + tamanho + "]";
+	}
+
 	public String[] getElementos() {
 		return elementos;
 	}
